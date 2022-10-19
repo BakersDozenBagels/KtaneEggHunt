c340560b64bd71d6642fb67c35814063a4d78d92
BakersDozenBagels <Bakersdozenbagelsbergman@gmail.com>
2022-10-19 18:14:22 -0400

    Egg Hunt

---
+++ Assets/EggHuntingScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EggHuntingScript : MonoBehaviour
+{
+    public Texture2D[] SpriteTextures;
+    public Renderer[] SpriteHolders;
+
+    public void Awake()
+    {
+        foreach(Renderer r in SpriteHolders)
+            r.enabled = false;
+    }
+
+    public void Start()
+    {
+        // Generate Race
+    }
+}
